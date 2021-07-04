2bebb829a2d8bc9fd7fbf1702b6595c225218239
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-04 05:25:52 -0300

    Create asd.js

---
+++ src/test/asd.js
@@ -0,0 +1,22 @@
+/*
+class HaxMap {
+  constructor (playersPerTeam, identifier) {
+    this.playersPerTeam = playersPerTeam;
+    this.identifier = identifier;
+    this.positionsRed = this.createPositionsArray(playersPerTeam);
+    this.positionsBlue = this.createPositionsArray(playersPerTeam);
+  }
+  createPositionsArray(length) {
+    let arr = [];
+    for (let i = 0; i < length; i++) arr[i] = 0;
+    return arr;
+  }
+}
+
+let randomMap = new HaxMap(3,'randomMap');
+
+const allowPositions = {
+  3 : 
+}
+
+*/
