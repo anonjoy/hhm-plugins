f0905baaf686be16032473939d2711c83d92a6af
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-06 20:33:49 -0300

    Update updp.js

--- src/test/disc-properties/updp.js
+++ src/test/disc-properties/updp.js
@@ -1,1 +1,17 @@
 
+var room = HBInit();
+
+room.pluginSpec = {
+  name    : `test/disc-properties/updp`, // Update Player Disc Properties
+};
+
+/* * * * * * * * * *  VARIABLES  * * * * * * * * * */
+
+/* * * * * * * * * *  FUNCIONES  * * * * * * * * * */
+
+/* * * * * * * * * * * EVENTOS * * * * * * * * * * */
+
+room.onGameStart        = onGameStartHandler;
+room.onPositionsReset   = onPositionsResetHandler;
+room.onPlayerTeamChange = onPlayerTeamChangeHandler;
+room.onPlayerLeave      = onPlayerLeaveHandler;
