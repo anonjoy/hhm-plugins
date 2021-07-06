af2560dc51b4545a3b62cf21c01e5e2dc98d9de3
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-05 22:27:07 -0300

    Update disc-properties-2.js

--- src/test/disc-properties-2.js
+++ src/test/disc-properties-2.js
@@ -13,36 +13,21 @@ room.pluginSpec = {
 
 /* * * * * * * * * *  FUNCIONES  * * * * * * * * * */
 
-function saveDefaultPlayerDiscProperties(DISC_PROPERTIES_OF_SOME_PLAYER){
-  
-}
-
-const non-persistent properties 
-
-function filterDefaultPlayerDiscProperties({radius,bCoeff,invMass,damping,...rest}){
-  return {radius,bCoeff,invMass,damping};
-}
-
 let catalog = [];
 
 class Match {
   constructor(){
     /* * VARIBABLES * */
-    const DEFAULT_STADIUMS                = [ "Classic", "Easy", "Small", "Big", "Rounded", "Hockey", "Big Hockey", "Big Easy", "Big Rounded", "Huge" ];
-    const DEFAULT_PLAYER_DISC_PROPERTIES  = { "radius" : 15, "bCoeff" : 0.5, "invMass" : 0.5, "damping" : 0.96 };
-    const DEFAULT_BALL_PROPERTIES         = { "radius" : 10, "bCoeff" : 0.5, "invMass" : 1, "damping" : 0.99, 
-                                              "color" : "FFFFFF", "cMask" : [ "all"], "cGroup" : [ "ball"] };
     let PLAYER_DISC_PROPERTIES;
     let BALL_PROPERTIES;
-    let STATE;
+    let GAME_STATE;
     let MAP_ID;
     let MAP_NAME;
     let MAP_WAS_SETTED;
-    let WAITING_FOR_PLAYERS;
     /* * * METODOS * * */
     this.setMap = function(index, force = false){
       if(!force){
-          switch(STATE){
+          switch(GAME_STATE){
             case 0: break; // GAME STOPPED
             case 1: case 2: return 1; // ERROR: GAME IS STARTED OR PAUSED
             default : return 2; // ERROR: PLUGIN NOT LOADED
@@ -57,62 +42,33 @@ class Match {
       PLAYER_DISC_PROPERTIES = {radius,bCoeff,invMass,damping};
     }
     this.getPlayerDiscProperties = () => PLAYER_DISC_PROPERTIES;
-    this.saveBallProperties = function({radius,bCoeff,invMass,damping,...rest}){
-      BALL_PROPERTIES = {radius,bCoeff,invMass,damping};
-    }
     this.getBallProperties = () => BALL_PROPERTIES;
     /* * * EVENTOS * * */
     this.onGameStart = function(){
-      if(PLAYER_DISC_PROPERTIES)
-      let player = room.getPlayerList().find((player) => player.team != 0);
-      if(player)  CM.savePlayerDiscProperties(getPlayerDiscProperties(player.id));
-      else        WAITING_FOR_PLAYERS = true;
-    }
-    this.onPlayerTeamChange = function(changedPlayer, byPlayer){
-      if(WAITING_FOR_PLAYERS){ 
-        CM.savePlayerDiscProperties(room.getPlayerDiscProperties(changedPlayer.id)); 
-        WAITING_FOR_PLAYERS = false;
+      if(!BALL_PROPERTIES) BALL_PROPERTIES = room.getDiscProperties(0);
+      if(!PLAYER_DISC_PROPERTIES){
+        let player = room.getPlayerList().find((player) => player.team != 0);
+        if(player) CM.savePlayerDiscProperties(getPlayerDiscProperties(player.id));
       }
     }
-    this.onGameStateChanged = (newState, ...args) => STATE = newState;
+    this.onPlayerTeamChange = function(changedPlayer, byPlayer){
+      if(!PLAYER_DISC_PROPERTIES && (GAME_STATE === 1 || GAME_STATE === 2) && changedPlayer.team != 0) CM.savePlayerDiscProperties(room.getPlayerDiscProperties(changedPlayer.id)); 
+    }
+    this.onGameStateChanged = (newState, ...args) => GAME_STATE = newState;
     this.onStadiumChange = function(newStadiumName, byPlayer){
-      MAP_NAME = newStadiumName;
-      /*
+      MAP_NAME                = newStadiumName;
+      PLAYER_DISC_PROPERTIES  = null;
+      BALL_PROPERTIES         = null;
       if(byPlayer)  MAP_WAS_SETTED  = true;
       else{         
                     MAP_WAS_SETTED  = false; 
                     MAP_ID          = null;
-      }
-      */
-      if(DEFAULT_STADIUMS.includes(MAP_NAME)){
-        PLAYER_DISC_PROPERTIES  = DEFAULT_PLAYER_DISC_PROPERTIES;
-        BALL_PROPERTIES         = DEFAULT_BALL_PROPERTIES;
-      }
-      else{
-        PLAYER_DISC_PROPERTIES  = null;
-        BALL_PROPERTIES         = null;
       }
     }
   }
 }
 
 const CM = new Match(); // Current Match
-
-
-function savePlayerDiscProperties(){
-  let properties;
-  if(mapWasSetted()) properties = getMapPlayersProperties();
-  else {
-    // IS THERE SOME PLAYER ON THE FIELD?
-    let player = room.getPlayerList().find((player) => player.team != 0);
-    if(player) match.map.saveDefaultPlayerDiscProperties(getPlayerDiscProperties(player.id));
-    else properties waitingForPlayers();
-  }
-}
-
-function onGameStartHandler(){
-  savePlayerDiscProperties();
-}
 
 /* * * * * * * * * * * EVENTOS * * * * * * * * * * */
 
