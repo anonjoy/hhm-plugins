69df665a1581a3c531eec5ce1a2ae25613d13527
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-06 20:14:05 -0300

    Update disc-properties-2.js

--- src/test/disc-properties-2.js
+++ src/test/disc-properties-2.js
@@ -24,14 +24,14 @@ function onGameStartHandler(){
   if(!BALL_PROPERTIES) BALL_PROPERTIES = room.getDiscProperties(0);
   if(!PLAYER_DISC_PROPERTIES){
     let player = room.getPlayerList().find((player) => player.team != 0);
-    if(player) PLAYER_DISC_PROPERTIES = (getPlayerDiscProperties(player.id)) => ({radius, bCoeff, invMass, damping});
+    if(player) PLAYER_DISC_PROPERTIES = (room.getPlayerDiscProperties(player.id)) => ({radius, bCoeff, invMass, damping});
   }
 }
 
 function onPlayerTeamChangeHandler(changedPlayer, byPlayer){
   if(!PLAYER_DISC_PROPERTIES){
     let GAME_STATE = room.getPlugin(`sav/game-state`).getGameState;
-    if(GAME_STATE && changedPlayer.team != 0) PLAYER_DISC_PROPERTIES = (getPlayerDiscProperties(changedPlayer.id)) => ({radius, bCoeff, invMass, damping});
+    if(GAME_STATE && changedPlayer.team != 0) PLAYER_DISC_PROPERTIES = (room.getPlayerDiscProperties(changedPlayer.id)) => ({radius, bCoeff, invMass, damping});
   }
 }
 
