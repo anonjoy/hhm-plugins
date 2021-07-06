3695a84fabe4524ba07f498f1c838edeb86c10f2
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-06 20:12:10 -0300

    Update disc-properties-2.js

--- src/test/disc-properties-2.js
+++ src/test/disc-properties-2.js
@@ -1,79 +1,42 @@
 var room = HBInit();
 
 room.pluginSpec = {
-  name    : `test/disc-properties`,
+  name    : `test/cmodp`, // Current Map Original Disc Properties
   config  : {},
 };
 
-/* * * * * * * * * * * CLASES * * * * * * * * * * */
-
-
 /* * * * * * * * * *  VARIABLES  * * * * * * * * * */
 
+let PLAYER_DISC_PROPERTIES;
+let BALL_PROPERTIES;
 
 /* * * * * * * * * *  FUNCIONES  * * * * * * * * * */
 
-let catalog = [];
+const getOriginalPlayerDiscProperties = () => PLAYER_DISC_PROPERTIES;
+const getOriginalBallProperties       = () => BALL_PROPERTIES;
 
-class Match {
-  constructor(){
-    /* * VARIBABLES * */
-    const DEFAULT_MAPS = ['Small', 'Easy', 'Classic', 'Big', 'Hockey', 'Rounded', 'Big Hockey', 'Big Easy', 'Big Rounded', 'Huge'];
-    let PLAYER_DISC_PROPERTIES;
-    let BALL_PROPERTIES;
-    let GAME_STATE;
-    let MAP_ID;
-    let MAP_NAME;
-    let MAP_WAS_SETTED;
-    /* * * METODOS * * */
-    this.setMap = function(index, force = false){
-      if(!force){
-          switch(GAME_STATE){
-            case 0: break; // GAME STOPPED
-            case 1: case 2: return 1; // ERROR: GAME IS STARTED OR PAUSED
-            default : return 2; // ERROR: PLUGIN NOT LOADED
-          }
-      }
-      else room.stopGame();
-      MAP_ID = index;
-      room.setCustomStadium(catalog[index].getStringCode);
-    }
-    this.getMap = () => MAP_ID;
-    this.savePlayerDiscProperties = function({radius,bCoeff,invMass,damping,...rest}){
-      PLAYER_DISC_PROPERTIES = {radius,bCoeff,invMass,damping};
-    }
-    this.getPlayerDiscProperties = () => PLAYER_DISC_PROPERTIES;
-    this.getBallProperties = () => BALL_PROPERTIES;
-    /* * * EVENTOS * * */
-    this.onGameStart = function(){
-      if(!BALL_PROPERTIES) BALL_PROPERTIES = room.getDiscProperties(0);
-      if(!PLAYER_DISC_PROPERTIES){
-        let player = room.getPlayerList().find((player) => player.team != 0);
-        if(player) CM.savePlayerDiscProperties(getPlayerDiscProperties(player.id));
-      }
-    }
-    this.onPlayerTeamChange = function(changedPlayer, byPlayer){
-      if(!PLAYER_DISC_PROPERTIES && (GAME_STATE === 1 || GAME_STATE === 2) && changedPlayer.team != 0) CM.savePlayerDiscProperties(room.getPlayerDiscProperties(changedPlayer.id)); 
-    }
-    this.onGameStateChanged = (newState, ...args) => GAME_STATE = newState;
-    this.onStadiumChange = function(newStadiumName, byPlayer){
-      MAP_NAME                = newStadiumName;
-      PLAYER_DISC_PROPERTIES  = null;
-      BALL_PROPERTIES         = null;
-      if(byPlayer){
-            MAP_WAS_SETTED    = false;
-            MAP_ID            = null;
-      }
-      else{
-            MAP_WAS_SETTED    = true; 
-            MAP_ID            = DEFAULT_MAPS.includes(MAP_NAME) ? null : MAP_ID;
-      }
-    }
+function onStadiumChangeHandler(newStadiumName, byPlayer){
+  PLAYER_DISC_PROPERTIES  = null;
+  BALL_PROPERTIES         = null;
+}
+
+function onGameStartHandler(){
+  if(!BALL_PROPERTIES) BALL_PROPERTIES = room.getDiscProperties(0);
+  if(!PLAYER_DISC_PROPERTIES){
+    let player = room.getPlayerList().find((player) => player.team != 0);
+    if(player) PLAYER_DISC_PROPERTIES = (getPlayerDiscProperties(player.id)) => ({radius, bCoeff, invMass, damping});
   }
 }
 
-const CM = new Match(); // Current Match
+function onPlayerTeamChangeHandler(changedPlayer, byPlayer){
+  if(!PLAYER_DISC_PROPERTIES){
+    let GAME_STATE = room.getPlugin(`sav/game-state`).getGameState;
+    if(GAME_STATE && changedPlayer.team != 0) PLAYER_DISC_PROPERTIES = (getPlayerDiscProperties(changedPlayer.id)) => ({radius, bCoeff, invMass, damping});
+  }
+}
 
 /* * * * * * * * * * * EVENTOS * * * * * * * * * * */
 
-room.onGameStart = onGameStartHandler;
+room.onStadiumChange    = onStadiumChangeHandler;
+room.onGameStart        = onGameStartHandler;
+room.onPlayerTeamChange = onPlayerTeamChangeHandler;
