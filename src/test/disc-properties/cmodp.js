ea3e71a76cff80cc0b7627c0bd160ae8c509931c
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-06 21:47:17 -0300

    Update cmodp.js

--- src/test/disc-properties/cmodp.js
+++ src/test/disc-properties/cmodp.js
@@ -7,32 +7,32 @@ room.pluginSpec = {
 
 /* * * * * * * * * *  VARIABLES  * * * * * * * * * */
 
-let PLAYER_DISC_PROPERTIES;
-let BALL_PROPERTIES;
+let ORIGINAL_PLAYER_DISC_PROPERTIES;
+let ORIGINAL_BALL_PROPERTIES;
 
 /* * * * * * * * * *  FUNCIONES  * * * * * * * * * */
 
 const filterProperties                = ({radius, bCoeff, invMass, damping}) => ({radius, bCoeff, invMass, damping});
-const getOriginalPlayerDiscProperties = () => PLAYER_DISC_PROPERTIES;
-const getOriginalBallProperties       = () => BALL_PROPERTIES;
+const getOriginalPlayerDiscProperties = () => ORIGINAL_PLAYER_DISC_PROPERTIES;
+const getOriginalBallProperties       = () => ORIGINAL_BALL_PROPERTIES;
 
 function onStadiumChangeHandler(newStadiumName, byPlayer){
-  PLAYER_DISC_PROPERTIES  = null;
-  BALL_PROPERTIES         = null;
+  ORIGINAL_PLAYER_DISC_PROPERTIES  = null;
+  ORIGINAL_BALL_PROPERTIES         = null;
 }
 
 function onGameStartHandler(){
-  if(!BALL_PROPERTIES) BALL_PROPERTIES = room.getDiscProperties(0);
-  if(!PLAYER_DISC_PROPERTIES){
+  if(!ORIGINAL_BALL_PROPERTIES) ORIGINAL_BALL_PROPERTIES = room.getDiscProperties(0);
+  if(!ORIGINAL_PLAYER_DISC_PROPERTIES){
     let player = room.getPlayerList().find((player) => player.team != 0);
-    if(player) PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(player.id));
+    if(player) ORIGINAL_PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(player.id));
   }
 }
 
 function onPlayerTeamChangeHandler(changedPlayer, byPlayer){
-  if(!PLAYER_DISC_PROPERTIES){
+  if(!ORIGINAL_PLAYER_DISC_PROPERTIES){
     let GAME_STATE = room.getPlugin(`sav/game-state`).getGameState;
-    if(GAME_STATE && changedPlayer.team != 0) PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(changedPlayer.id));
+    if(GAME_STATE && changedPlayer.team != 0) ORIGINAL_PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(changedPlayer.id));
   }
 }
 
