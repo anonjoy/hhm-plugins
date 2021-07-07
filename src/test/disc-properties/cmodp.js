609ccefe4cf6fe04171d0538327dc8d590e1a3a0
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-07 02:26:42 -0300

    Update cmodp.js

--- src/test/disc-properties/cmodp.js
+++ src/test/disc-properties/cmodp.js
@@ -7,32 +7,51 @@ room.pluginSpec = {
 
 /* * * * * * * * * *  VARIABLES  * * * * * * * * * */
 
-let ORIGINAL_PLAYER_DISC_PROPERTIES;
+const TEAM = { SPEC: 0, RED: 1, BLUE: 2};
+let ORIGINAL_PLAYER_DISC_PROPERTIES = {};
+
+
+
 let ORIGINAL_BALL_PROPERTIES;
 
 /* * * * * * * * * *  FUNCIONES  * * * * * * * * * */
 
-const filterProperties                = ({radius, bCoeff, invMass, damping}) => ({radius, bCoeff, invMass, damping});
-const getOriginalPlayerDiscProperties = () => ORIGINAL_PLAYER_DISC_PROPERTIES;
+function filterProperties(PLAYER_TEAM, {x, y, xspeed, yspeed, color, ...PLAYER_DISC_PROPERTIES}){
+  switch(PLAYER_TEAM){
+    case 1: case 2: return PLAYER_DISC_PROPERTIES;
+  }
+}
+
+const getOriginalPlayerDiscProperties = (PLAYER_TEAM) => ORIGINAL_PLAYER_DISC_PROPERTIES[PLAYER_TEAM];
 const getOriginalBallProperties       = () => ORIGINAL_BALL_PROPERTIES;
 
 function onStadiumChangeHandler(newStadiumName, byPlayer){
-  ORIGINAL_PLAYER_DISC_PROPERTIES  = null;
-  ORIGINAL_BALL_PROPERTIES         = null;
+  ORIGINAL_PLAYER_DISC_PROPERTIES = {}
+  ORIGINAL_BALL_PROPERTIES        = null;
 }
 
 function onGameStartHandler(){
   if(!ORIGINAL_BALL_PROPERTIES) ORIGINAL_BALL_PROPERTIES = room.getDiscProperties(0);
-  if(!ORIGINAL_PLAYER_DISC_PROPERTIES){
+  if(ORIGINAL_PLAYER_DISC_PROPERTIES === {}){
     let player = room.getPlayerList().find((player) => player.team != 0);
-    if(player) ORIGINAL_PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(player.id));
+  }
+  if(!ORIGINAL_PLAYER_DISC_PROPERTIES[TEAM.RED] || !ORIGINAL_PLAYER_DISC_PROPERTIES[TEAM.BLUE]){
+    let player = room.getPlayerList().find((player) => player.team != 0);
+    if(player) ORIGINAL_PLAYER_DISC_PROPERTIES[player.team] = filterProperties(room.getPlayerDiscProperties(player.id));
   }
 }
 
 function onPlayerTeamChangeHandler(changedPlayer, byPlayer){
-  if(!ORIGINAL_PLAYER_DISC_PROPERTIES){
+  if(!ORIGINAL_PLAYER_DISC_PROPERTIES[changedPlayer.team]){
+  }
+  if(!getOriginalPlayerDiscProperties(changedPlayer.team)){
     let GAME_STATE = room.getPlugin(`sav/game-state`).getGameState();
-    if(GAME_STATE && changedPlayer.team != 0) ORIGINAL_PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(changedPlayer.id));
+    if(GAME_STATE){
+      switch(changedPlayer.team){
+        case 1: ORIGINAL_RED_PLAYER_DISC_PROPERTIES   = filterProperties(room.getPlayerDiscProperties(changedPlayer.id));
+        case 2: ORIGINAL_BLUE_PLAYER_DISC_PROPERTIES  = filterProperties(room.getPlayerDiscProperties(changedPlayer.id));
+      }
+    }
   }
 }
 
