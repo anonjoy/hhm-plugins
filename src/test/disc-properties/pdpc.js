e9a0aed5fd6b35a2685ffb5d692b5877ada85d5e
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-06 22:04:01 -0300

    Update pdpc.js

--- src/test/disc-properties/pdpc.js
+++ src/test/disc-properties/pdpc.js
@@ -7,37 +7,160 @@ room.pluginSpec = {
 
 /* * * * * * * * * *  VARIABLES  * * * * * * * * * */
 
-let ORIGINAL_PLAYER_DISC_PROPERTIES;
-let ORIGINAL_BALL_PROPERTIES;
-
 /* * * * * * * * * *  FUNCIONES  * * * * * * * * * */
 
-const filterProperties                = ({radius, bCoeff, invMass, damping}) => ({radius, bCoeff, invMass, damping});
-const getOriginalPlayerDiscProperties = () => ORIGINAL_PLAYER_DISC_PROPERTIES;
-const getOriginalBallProperties       = () => ORIGINAL_BALL_PROPERTIES;
+const discProperties = {
+  'x' : `float`,
+  'y' : `float`,
+  'xspeed' : `float`,
+  'yspeed' : `float`,
+  'xgravity' : `float`,
+  'ygravity' : `float`,
+  'radius' : `float`,
+  'bCoeff' : `float`,
+  'invMass' : `float`,
+  'damping' : `float`,
+  'color' : `int`,
+  'cMask' : `int`,
+  'cGroup' : `int`, 
+  
+};
 
-function onStadiumChangeHandler(newStadiumName, byPlayer){
-  ORIGINAL_PLAYER_DISC_PROPERTIES  = null;
-  ORIGINAL_BALL_PROPERTIES         = null;
-}
+let playersDiscProperties = {};
 
-function onGameStartHandler(){
-  if(!ORIGINAL_BALL_PROPERTIES) ORIGINAL_BALL_PROPERTIES = room.getDiscProperties(0);
-  if(!ORIGINAL_PLAYER_DISC_PROPERTIES){
-    let player = room.getPlayerList().find((player) => player.team != 0);
-    if(player) ORIGINAL_PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(player.id));
+function updateDiscProperties () {
+  for ( let [key, value] of Object.entries( playersDiscProperties ) ) {
+    room.setPlayerDiscProperties(key, value);
   }
 }
 
-function onPlayerTeamChangeHandler(changedPlayer, byPlayer){
-  if(!ORIGINAL_PLAYER_DISC_PROPERTIES){
-    let GAME_STATE = room.getPlugin(`sav/game-state`).getGameState;
-    if(GAME_STATE && changedPlayer.team != 0) ORIGINAL_PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(changedPlayer.id));
+function onCommandDiscPropertiesHandler ( id, properties ) {
+  if ( !playersDiscProperties[id] ) playersDiscProperties[id] = {};
+  for ( let [key, value] of Object.entries( properties ) ) {
+    if (!config.updateExceptions.includes(key)) playersDiscProperties[id][key] = value;
+  }
+  let state;
+  state = getGameState();
+  if ( state == 1 || state == 2 ) {
+    room.setPlayerDiscProperties( id, properties );
   }
 }
+
+const onCommandDiscHandlerData = {
+  'sav/help': {
+    text: ` [DISC_PROPERTY VALUE] PLAYER_ID (OPTIONAL), to change the properties of a player's body (Change your body if you don't pass PLAYER_ID parameter)`,
+    roles: config.allowedRoles,
+  },
+};
+
+const error = {
+  1 : (id, property) => {room.sendAnnouncement(`The argument ` + property + ` is not a valid property of discs.`, id, config.format.error);},
+  2 : (id, property) => {room.sendAnnouncement(`The VALUE argument of the property ` + property + ` is not a valid value.`, id, config.format.error);},
+  3 : (id, argID) => {room.sendAnnouncement(`The argument ` + argID + ` is not a valid ID.`, id, config.format.error);},
+  4 : (id) => {room.sendAnnouncement(`DISC_PROPERTY and VALUE parameters are necessary.`, id, config.format.error);},
+  5 : (id) => {room.sendAnnouncement(`The argument must be a number between ` + config.defaultMinSize + ` and ` + config.defaultMaxSize + `.`, id, config.format.error);},
+};
+
+function onCommandDiscHandler ( player, arguments, argumentString ) {
+  if (!arguments[0] || !arguments[1]) return error[4](player.id);
+  let properties = {};
+  let property;
+  let value;
+  let length = arguments.length % 2 != 0 ? arguments.length - 1 : arguments.length;
+  for (let i = 0; i < length; i+=2) {
+    property = arguments[i];
+    value = parseFloat(arguments[i+1]);
+    if (!(property in discProperties)) {
+      error[1](player.id, property);
+      continue;
+    }
+    if (isNaN(value)) {
+      error[2](player.id, property);
+      continue;
+    }
+    if (discProperties[property] == `int` && !Number.isInteger(value)) {
+      error[2](player.id, property);
+      continue;
+    }
+    properties[property] = value;
+  }
+  let id = arguments.length % 2 != 0 ? arguments[arguments.length - 1] : player.id;
+  !isNaN(id) && room.getPlayerList().some((player) => player.id == id) ? onCommandDiscPropertiesHandler(id, properties) : error[3](player.id, id);
+  return false;
+}
+
+const onCommandSizeHandlerData = {
+  'sav/help': {
+    text: ` SIZE_NUMBER, to change the size of your body.`,
+    roles: config.defaultRole,
+  },
+};
+
+function onCommandSizeHandler ( player, arguments, argumentString ) {
+  
+  let argument = parseInt(arguments[0]);
+  player.roles = roles.getPlayerRoles(player.id);
+  
+  if ( isNaN(argument) ) error[5](player.id);
+  else if ( argument >= config.defaultMinSize && argument <= config.defaultMaxSize ) {
+    onCommandDiscPropertiesHandler( player.id, { 'radius' : argument } );
+  }
+  else if ( config.allowedRoles.some((role) => player.roles.includes(role)) ) {
+    onCommandDiscPropertiesHandler( player.id, { 'radius' : argument } );
+  }
+  else error[5](player.id);
+  return false;
+}
+
+const onCommandDiscResetHandlerData = {
+  'sav/help': {
+    text: ` PLAYER_ID/all (OPTIONAL), to reset the properties of player's body. (Reset your properties if you don't pass any parameter)`,
+    roles: config.allowedRoles,
+  },
+};
+
+function onCommandDiscResetHandler ( player, arguments, argumentString ) {
+  let argument = arguments[0];
+  if (!argument && playersDiscProperties[player.id]) delete playersDiscProperties[player.id];
+  else if ( isNaN(argument) && argument == 'all' ) playersDiscProperties = {};
+  else !isNaN(argument) && room.getPlayerList().some((player) => player.id == argument) ? delete playersDiscProperties[argument] : error[3](player.id, argument);
+  return false;
+}
+
+function onPlayerLeaveHandler ( player ) {
+  if (playersDiscProperties[player.id]) delete playersDiscProperties[player.id];
+}
+
+function onGameStartHandler () {
+  updateDiscProperties();
+}
+
+function onPositionsResetHandler () {
+  updateDiscProperties();
+}
+
+function onPlayerTeamChangeHandler ( player ) {
+  let state = getGameState();
+  if ( state == 1 || state == 2  ) updateDiscProperties();
+}
+
+room.onCommand1_size = {
+  function: onCommandSizeHandler,
+  data: onCommandSizeHandlerData,
+};
+room.onCommand_disc = {
+  function: onCommandDiscHandler,
+  data: onCommandDiscHandlerData,
+};
+room.onCommand_disc_reset = {
+  function: onCommandDiscResetHandler,
+  data: onCommandDiscResetHandlerData,
+};
+room.onPlayerLeave = onPlayerLeaveHandler;
+room.onGameStart = onGameStartHandler;
+room.onPositionsReset = onPositionsResetHandler;
+room.onPlayerTeamChange = onPlayerTeamChangeHandler;
+
 
 /* * * * * * * * * * * EVENTOS * * * * * * * * * * */
 
-room.onStadiumChange    = onStadiumChangeHandler;
-room.onGameStart        = onGameStartHandler;
-room.onPlayerTeamChange = onPlayerTeamChangeHandler;
