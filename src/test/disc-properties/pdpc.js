153fa0b16e4159c936e61e745b7095aed2a1e684
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-06 22:02:55 -0300

    Create pdpc.js

---
+++ src/test/disc-properties/pdpc.js
@@ -0,0 +1,43 @@
+
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `test/disc-properties/pdpc`, // Player Disc Properties Commands
+};
+
+/* * * * * * * * * *  VARIABLES  * * * * * * * * * */
+
+let ORIGINAL_PLAYER_DISC_PROPERTIES;
+let ORIGINAL_BALL_PROPERTIES;
+
+/* * * * * * * * * *  FUNCIONES  * * * * * * * * * */
+
+const filterProperties                = ({radius, bCoeff, invMass, damping}) => ({radius, bCoeff, invMass, damping});
+const getOriginalPlayerDiscProperties = () => ORIGINAL_PLAYER_DISC_PROPERTIES;
+const getOriginalBallProperties       = () => ORIGINAL_BALL_PROPERTIES;
+
+function onStadiumChangeHandler(newStadiumName, byPlayer){
+  ORIGINAL_PLAYER_DISC_PROPERTIES  = null;
+  ORIGINAL_BALL_PROPERTIES         = null;
+}
+
+function onGameStartHandler(){
+  if(!ORIGINAL_BALL_PROPERTIES) ORIGINAL_BALL_PROPERTIES = room.getDiscProperties(0);
+  if(!ORIGINAL_PLAYER_DISC_PROPERTIES){
+    let player = room.getPlayerList().find((player) => player.team != 0);
+    if(player) ORIGINAL_PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(player.id));
+  }
+}
+
+function onPlayerTeamChangeHandler(changedPlayer, byPlayer){
+  if(!ORIGINAL_PLAYER_DISC_PROPERTIES){
+    let GAME_STATE = room.getPlugin(`sav/game-state`).getGameState;
+    if(GAME_STATE && changedPlayer.team != 0) ORIGINAL_PLAYER_DISC_PROPERTIES = filterProperties(room.getPlayerDiscProperties(changedPlayer.id));
+  }
+}
+
+/* * * * * * * * * * * EVENTOS * * * * * * * * * * */
+
+room.onStadiumChange    = onStadiumChangeHandler;
+room.onGameStart        = onGameStartHandler;
+room.onPlayerTeamChange = onPlayerTeamChangeHandler;
