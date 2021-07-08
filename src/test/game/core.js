b3c8fe7e3e159fbbb8c9c44ec449b0e9dcd61026
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-08 00:39:33 -0300

    Update core.js

--- src/test/game/core.js
+++ src/test/game/core.js
@@ -7,17 +7,17 @@ room.pluginSpec = {
 
 class Game {
   constructor(){
-    let GAME_STATE = 0;
-    let PREVIOUS_GAME_STATE;
+    let STATE = 0;
+    let PREVIOUS_STATE;
     let ORIGINAL_BALL_PROPERTIES;
     let ORIGINAL_PLAYER_DISC_PROPERTIES = {};
-    this.getGameState                     = () => GAME_STATE;
-    this.getPreviousGameState             = () => PREVIOUS_GAME_STATE;
+    this.getState                         = () => STATE;
+    this.getPreviousState                 = () => PREVIOUS_STATE;
     this.getOriginalBallProperties        = () => ORIGINAL_BALL_PROPERTIES;
     this.getOriginalPlayerDiscProperties  = (TEAM) => ORIGINAL_PLAYER_DISC_PROPERTIES[TEAM];
     this.setGameState = function(NEW_STATE){
-      PREVIOUS_GAME_STATE = GAME_STATE;
-      GAME_STATE = NEW_STATE;
+      PREVIOUS_STATE = STATE;
+      STATE = NEW_STATE;
     }
     this.setOriginalBallProperties = function(BALL_PROPERTIES){
       ORIGINAL_BALL_PROPERTIES = BALL_PROPERTIES;
@@ -28,5 +28,5 @@ class Game {
   }
 }
 
-const GM = new Game();
-const getGameObject = () => GM;
+const GAME = new Game();
+const getGameObject = () => GAME;
