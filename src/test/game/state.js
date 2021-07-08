a663b21e2832c163b2c724025a32de0e6ec5f840
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-08 00:03:38 -0300

    Update state.js

--- src/test/game/state.js
+++ src/test/game/state.js
@@ -5,44 +5,15 @@ room.pluginSpec = {
   name: `...`,
 };
 
+const GM = room.getPlugin(`test/game/core`).getGameObject();
+
 const STATES = {
   STOPPED: 0,
   STARTED: 1,
   PAUSED: 2,
 };
 
-function onGameStartHandler(...args) {
-  triggerGameStateChange(states.STARTED, `onGameStart`, ...args);
-}
-
-function onGameStopHandler(...args) {
-  triggerGameStateChange(states.STOPPED, `onGameStop`, ...args);
-}
-
-function onGamePauseHandler(...args) {
-  triggerGameStateChange(states.PAUSED, `onGamePause`, ...args);
-}
-
-function onGameUnpauseHandler(...args) {
-  triggerGameStateChange(states.STARTED, `onGameUnpause`, ...args);
-}
-
-function triggerGameStateChange(newState, nativeEventName, ...args) {
-  const previousState = state;
-  state = newState;
-
-  room.triggerEvent(`onGameStateChanged`, newState, previousState,
-      { name: nativeEventName, args });
-}
-
-function getGameState() {
-  return state;
-}
-
-
-// room.states = states;
-room.onGameStart = onGameStartHandler;
-room.onGameStop = onGameStopHandler;
-room.onGamePause = onGamePauseHandler;
-room.onGameUnpause = onGameUnpauseHandler;
-room.getGameState = getGameState;
+room.onGameStart    = () => GM.setGameState(STATES.STARTED);
+room.onGameStop     = () => GM.setGameState(STATES.STOPPED);
+room.onGamePause    = () => GM.setGameState(STATES.PAUSED);
+room.onGameUnpause  = () => GM.setGameState(STATES.STARTED);
