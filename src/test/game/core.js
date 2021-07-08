8731b297d7bbf1ffbdd7586592a2f186bfa0290f
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-07 23:42:43 -0300

    Update core.js

--- src/test/game/core.js
+++ src/test/game/core.js
@@ -1,1 +1,15 @@
 
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `...`, // Current Map Original Disc Properties
+};
+
+class Game {
+  constructor(){
+    let GAME_STATE;
+    this.getGameState = () => GAME_STATE;
+  }
+}
+
+const GM = new Game();
