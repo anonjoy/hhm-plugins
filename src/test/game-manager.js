7f3956262a4a431b7740d5bd1ea98d0a1c34b969
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-07 23:38:13 -0300

    Create game-manager.js

---
+++ src/test/game-manager.js
@@ -0,0 +1,46 @@
+var room = HBInit();
+
+room.pluginSpec = {
+  name: `...`,
+};
+
+const STATE = {
+  STOPPED: 0,
+  STARTED: 1,
+  PAUSED: 2,
+};
+
+function onGameStartHandler(...args) {
+  triggerGameStateChange(states.STARTED, `onGameStart`, ...args);
+}
+
+function onGameStopHandler(...args) {
+  triggerGameStateChange(states.STOPPED, `onGameStop`, ...args);
+}
+
+function onGamePauseHandler(...args) {
+  triggerGameStateChange(states.PAUSED, `onGamePause`, ...args);
+}
+
+function onGameUnpauseHandler(...args) {
+  triggerGameStateChange(states.STARTED, `onGameUnpause`, ...args);
+}
+
+function triggerGameStateChange(newState, nativeEventName, ...args) {
+  const previousState = state;
+  state = newState;
+
+  room.triggerEvent(`onGameStateChanged`, newState, previousState,
+      { name: nativeEventName, args });
+}
+
+function getGameState() {
+  return state;
+}
+
+room.states = states;
+room.onGameStart = onGameStartHandler;
+room.onGameStop = onGameStopHandler;
+room.onGamePause = onGamePauseHandler;
+room.onGameUnpause = onGameUnpauseHandler;
+room.getGameState = getGameState;
