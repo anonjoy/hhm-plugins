5a1308412c445333aee606a1db349aee1dc21839
ayudantehax <86815883+ayudantehax@users.noreply.github.com>
2021-07-07 23:59:28 -0300

    Update core.js

--- src/test/game/core.js
+++ src/test/game/core.js
@@ -7,10 +7,11 @@ room.pluginSpec = {
 
 class Game {
   constructor(){
-    let GAME_STATE;
+    let GAME_STATE = 0;
     let PREVIOUS_GAME_STATE;
     this.getGameState = () => GAME_STATE;
     this.setGameState = function(NEW_STATE){
+      PREVIOUS_GAME_STATE = GAME_STATE;
       GAME_STATE = NEW_STATE;
     }
   }
